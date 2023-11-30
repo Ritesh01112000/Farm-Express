3277db0c382232e73117e650227029116e82c171
Ritesh <rsv@skyrist.com>
2023-11-30 18:49:12 +0530

    Fixed Imports

--- app/src/pages/dashboard/dashboard.js
+++ app/src/pages/dashboard/dashboard.js
@@ -1,6 +1,5 @@
 import React,{useEffect, useState} from "react";
 import { Link } from "react-router-dom";
-import image from "../../assets/FarmRed.jpg";
 import Navbar from "../../components/navbar/navbar";
 import Dialogue from "../machineDetails/dialogue";
 import { useSelector, useDispatch } from "react-redux";
